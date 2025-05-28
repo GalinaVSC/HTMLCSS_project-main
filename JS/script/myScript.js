e3d25dbc89220647f2f0cb9a340a9ccb141f2a35
Светлана <haossvet@gmail.com>
2025-05-28 16:06:07 +1000

    Add files via upload
    новый материал

---
+++ JS/script/myScript.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Что для меня утро, это время прогулок и размышлений, время общения и позитива, время наслаждением звуками природы";
+}
+
+function myFunctionImage() {
+    document.getElementById("demo").innerHTML = "paragraph changed.";
+}
