ce82d37b3569c05035ef434762540403ab6c59d4
galinavsc <haossvet@gmail.com>
2025-05-28 20:30:07 +1000

    дз

--- JS/script/myScript.js
+++ JS/script/myScript.js
@@ -1,7 +1,14 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Что для меня утро, это время прогулок и размышлений, время общения и позитива, время наслаждением звуками природы";
+    document.getElementById("demo").innerHTML = "Что для меня утро, это время прогулок и размышлений, время общения и позитива, время наслаждения звуками природы";
 }
 
-function myFunctionImage() {
-    document.getElementById("demo").innerHTML = "paragraph changed.";
+function myFunctionImg() {
+    document.getElementById('myImage').src ='img/utro.jpg';
 }
+
+//function myFunctionImg() {
+    //const image = document.createElement('img');      // создаем элемент img
+   // image.src = 'img/utro.jpg';                  // задаём адрес изображения
+   // image.alt = 'Утренние лучи сквозь деревья';     // альтернативный текст для доступности
+   // document.getElementById('image').appendChild(image); // добавляем изображение в контейнер
+//}
