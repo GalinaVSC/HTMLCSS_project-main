44c7b69763a3f23c274966568f328928f3a4af20
galinavsc <haossvet@gmail.com>
2025-06-02 21:40:54 +1000

    исправление грамматической ошибки

--- JS/declarations/constArray.js
+++ JS/declarations/constArray.js
@@ -1,7 +1,7 @@
 const person = { name: "Alex", age: 25};
-person.age = 26; //Alloved
+person.age = 26; //Allowed
 console.log(person.age); // 26
 
 const numbers = [1, 2, 3];
-numbers.push(4); // allowed
+numbers.push(4); // Allowed
 console.log(numbers); // [1, 2, 3, 4]
