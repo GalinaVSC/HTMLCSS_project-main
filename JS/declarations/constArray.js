ed844f7d2d0d63ca5f41d8e70b5b850c3a70aab1
galinavsc <haossvet@gmail.com>
2025-06-02 20:29:59 +1000

    декларации переменных

---
+++ JS/declarations/constArray.js
@@ -0,0 +1,7 @@
+const person = { name: "Alex", age: 25};
+person.age = 26; //Alloved
+console.log(person.age); // 26
+
+const numbers = [1, 2, 3];
+numbers.push(4); // allowed
+console.log(numbers); // [1, 2, 3, 4]
