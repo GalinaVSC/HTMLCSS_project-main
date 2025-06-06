35cfadaf01c9da83b198497050ac996d8d3d5597
galinavsc <haossvet@gmail.com>
2025-06-06 21:23:06 +1000

    добавлено в калькулятор умножение и деление

--- JS/syntax/calculator.js
+++ JS/syntax/calculator.js
@@ -1,13 +1,26 @@
 function addition() {
     let b = Number(document.getElementById("firstSum").value);
     let c = Number(document.getElementById("secondSum").value);
-    let a = b + c;
-    document.getElementById("demoPlus").innerHTML = a;
+    let sum = b + c;
+    document.getElementById("demoPlus").textContent = sum;
 }
         
 function difference() {
     let b = Number(document.getElementById("firstDiff").value);
     let c = Number(document.getElementById("secondDiff").value);
-    let a = b - c;
-    document.getElementById("demoMinus").innerHTML = a;
+    let diff = b - c;
+    document.getElementById("demoMinus").innerHTML = diff;
+}
+
+function multiplication() {
+    let b = Number(document.getElementById("firstMult").value);
+    let c = Number(document.getElementById("secondMult").value);
+    let mult = b * c;
+    document.getElementById("demoMult").innerHTML = mult;
+}
+function division() {
+    let b = Number(document.getElementById("firstDivi").value);
+    let c = Number(document.getElementById("secondDivi").value);
+    let divi= b / c;
+    document.getElementById("demoDivi").innerHTML = divi;
 }
