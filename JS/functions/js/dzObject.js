0fd5c52f4ea27d88495a81741138ac98c4b839e0
galinavsc <haossvet@gmail.com>
2025-06-11 22:50:03 +1000

    dz

--- JS/functions/js/dzObject.js
+++ JS/functions/js/dzObject.js
@@ -1,26 +1,27 @@
 function validateForm() {
-    const person = {
-        firstName:document.getElementById("firstname").value,
-        lastName:document.getElementById("lastname").value,
-        email:document.getElementById("email").value
-       };
-        if (typeof firstName !== "string" || !isNaN(firstName)) {
+    const person = {};
+        person.firstName=document.getElementById("firstname").value;
+        person.lastName=document.getElementById("lastname").value;
+        person.email=document.getElementById("email").value;
+       
+        if (typeof person.firstName !== "string" || !isNaN(firstName)) {
            alert("First Name cannot contain numbers!");
            return false; 
         }
-        if (typeof lastName !== "string" || !isNaN(lastName)) {
+        if (typeof person.lastName !== "string" || !isNaN(lastName)) {
             alert("Last Name cannot contain numbers!");
             return false;
         }
-        if (typeof email !== "string" || !email.includes("@")) {
+        if (typeof person.email !== "string" || !email.includes("@")) {
             alert("Enter valid email!");
             return false;
         }
        
-        document.getElementById("output").innerHTML = 
-        "<p><strong>First Name:</strong></p>" +
-        "<p><strong>Last Name:</strong></p>"  +
-        "<p><strong>Email:</strong></p>";
+        document.getElementById("output").innerHTML = `
+        <p><strong>First Name:</strong> ${person.firstName}</p>
+        <p><strong>Last Name:</strong> ${person.lastName}</p>
+        <p><strong>Email:</strong> ${person.email}</p>
+        `;
 
         return false; // Prevent actual form submission Предотвратить фактическую отправку формы
     }
